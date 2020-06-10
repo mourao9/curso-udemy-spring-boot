bf49db9f5ae959a1a2dd4c5017dd9dd6d057098f
mourao9 <elvismourao@hotmail.com>
2020-06-09 22:36:32 -0300

    exceção delete handling

--- src/main/java/com/mourao/curso/services/UsuarioService.java
+++ src/main/java/com/mourao/curso/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mourao.curso.entities.Usuario;
 import com.mourao.curso.repositories.UserRepository;
+import com.mourao.curso.services.exceptions.DataBaseException;
 import com.mourao.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UsuarioService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public Usuario atualizar(Long id, Usuario usuario) {
